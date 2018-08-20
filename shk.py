65151e74c718ff6968c8acffc86fab0b7217e9f3
Shuichi Aizawa <sa396@columbia.edu>
2018-08-20 00:28:03 -0400

    more specterkiller

--- shk.py
+++ shk.py
@@ -8,7 +8,7 @@ class AlgoStrategy(gamelib.AlgoCore):
 		super().__init__()
 		random.seed()
 		gamelib.debug_write("init")
-		self.mode = 1
+		self.mode = 1	# hounddog
 
 	def process_config(self, config):
 		gamelib.debug_write("config")
@@ -23,11 +23,13 @@ class AlgoStrategy(gamelib.AlgoCore):
 		defe_locs = [[
 			"DF",
 			[[11,11],[16,11],[12,11],[15,11],[13,11],[14,11],[12,12],[15,12],[13,12],[14,12],[13,13],[14,13]],
+			[[24,13],[23,13],[23,12],[22,13],[22,12],[22,11],[21,13],[21,12],[20,13]],
 			[[0,13],[1,13],[1,12],[2,13],[2,12],[2,11],[3,13],[3,12],[4,13]],
 			[[27,13],[26,13],[26,12],[25,13],[25,12],[25,11],[24,13],[24,12],[23,13]]
 		],[
 			"FF",
 			[[10,11],[17,11],[11,12],[16,12],[12,13],[15,13],[0,13],[1,12],[2,11],[3,10],[4,9],[5,8],[6,7],[7,6],[8,5],[9,4],[10,3],[11,2]],
+			[[19,13],[20,12],[21,11],[0,13],[1,12],[2,11],[3,10],[4,9],[5,8],[6,7],[7,6],[8,5],[9,4],[10,3],[11,2]],
 			[[5,13],[4,12],[3,11]],
 			[[22,13],[23,12],[24,11]]
 		]]
@@ -38,28 +40,36 @@ class AlgoStrategy(gamelib.AlgoCore):
 			"FF",
 			filt_locs,
 			filt_locs,
+			filt_locs,
 			[[3,13],[4,12],[5,11],[6,10],[7,9],[8,8],[9,7],[10,6],[11,5],[12,4],[13,3],[14,2],[15,1]]
 		],[
 			"EF",
+			encr_locs,
 			encr_locs,
 			encr_locs,
 			[[2,13],[3,12],[4,11],[5,10],[6,9],[7,8],[8,7],[9,6],[10,5],[11,4],[12,3],[13,2],[14,1]]
 		]]
 
 		if self.mode == 1:
-			for y in range(14,17):
-				if len(game_map.filter_blocked_locations([[12,y],[13,y],[14,y],[15,y]])) == 0:
-					if len(game_map.filter_blocked_locations([[26,14],[27,14]])) == 0:
-						for defe in defe_locs:
-							game_map.attempt_remove_multiple(defe[self.mode])
-						self.mode = 2
-					elif len(game_map.filter_blocked_locations([[0,14],[1,14]])) == 0:
-						for defe in defe_locs:
-							game_map.attempt_remove_multiple(defe[self.mode])
-						for offe in offe_locs:
-							game_map.attempt_remove_multiple(offe[self.mode])
-						self.mode = 3
-					gamelib.debug_write("mode:{}".format(self.mode))
+			if len(game_map.filter_blocked_locations(game_map.get_edge_locations("top_left") + game_map.get_edge_locations("top_right"))) <= 4:
+				for defe in defe_locs:
+					game_map.attempt_remove_multiple(defe[self.mode])
+				self.mode = 2 # specter
+				gamelib.debug_write("mode:{}".format(self.mode))
+			else:
+				for y in range(14,17):
+					if len(game_map.filter_blocked_locations([[12,y],[13,y],[14,y],[15,y]])) == 0:
+						if len(game_map.filter_blocked_locations([[26,14],[27,14]])) == 0:
+							for defe in defe_locs:
+								game_map.attempt_remove_multiple(defe[self.mode])
+							self.mode = 3	# starfish
+						elif len(game_map.filter_blocked_locations([[0,14],[1,14]])) == 0:
+							for defe in defe_locs:
+								game_map.attempt_remove_multiple(defe[self.mode])
+							for offe in offe_locs:
+								game_map.attempt_remove_multiple(offe[self.mode])
+							self.mode = 4	# champ
+						gamelib.debug_write("mode:{}".format(self.mode))
 
 		cores = game_map.get_resource("cores") - len(game_map.filter_blocked_locations(offe_locs[0][self.mode])) * 1.5
 		for defe in defe_locs:
