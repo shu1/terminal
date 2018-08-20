4e850ca1d6db7af442f7c342fc0f3c28dd6594c3
Shuichi Aizawa <sa396@columbia.edu>
2018-08-19 20:57:48 -0400

    code cleanup

--- shk.py
+++ shk.py
@@ -22,26 +22,29 @@ class AlgoStrategy(gamelib.AlgoCore):
 
 		filt_locs = [[24,13],[23,12],[22,11],[21,10],[20,9],[19,8],[18,7],[17,6],[16,5],[15,4],[14,3],[13,2],[12,1]]
 		encr_locs = [[25,13],[24,12],[23,11],[22,10],[21,9],[20,8],[19,7],[18,6],[17,5],[16,4],[15,3],[14,2],[13,1]]
-		offe_locs = [
-			["FF",
+		offe_locs = [[
+			"FF",
 			filt_locs,
 			filt_locs,
-			[[3,13],[4,12],[5,11],[6,10],[7,9],[8,8],[9,7],[10,6],[11,5],[12,4],[13,3],[14,2],[15,1]]],
-			["EF",
+			[[3,13],[4,12],[5,11],[6,10],[7,9],[8,8],[9,7],[10,6],[11,5],[12,4],[13,3],[14,2],[15,1]]
+		],[
+			"EF",
 			encr_locs,
 			encr_locs,
-			[[2,13],[3,12],[4,11],[5,10],[6,9],[7,8],[8,7],[9,6],[10,5],[11,4],[12,3],[13,2],[14,1]]]
-		]
-		defe_locs = [
-			["DF",
-			[[13,13],[14,13],[12,12],[15,12],[13,12],[14,12],[11,11],[16,11],[12,11],[15,11],[13,11],[14,11]],
+			[[2,13],[3,12],[4,11],[5,10],[6,9],[7,8],[8,7],[9,6],[10,5],[11,4],[12,3],[13,2],[14,1]]
+		]]
+
+		defe_locs = [[
+			"DF",
+			[[11,11],[16,11],[12,11],[15,11],[13,11],[14,11],[12,12],[15,12],[13,12],[14,12],[13,13],[14,13]],
 			[[0,13],[1,13],[1,12],[2,13],[2,12],[2,11],[3,13],[3,12],[4,13]],
-			[[27,13],[26,13],[26,12],[25,13],[25,12],[25,11],[24,13],[24,12],[23,13]]],
-			["FF",
-			[[12,13],[15,13],[11,12],[16,12],[10,11],[17,11]],
+			[[27,13],[26,13],[26,12],[25,13],[25,12],[25,11],[24,13],[24,12],[23,13]]
+		],[
+			"FF",
+			[[10,11],[17,11],[11,12],[16,12],[12,13],[15,13],[3,13],[4,12],[5,11],[6,10],[7,9]],
 			[[5,13],[4,12],[3,11]],
-			[[22,13],[23,12],[24,11]]]
-		]
+			[[22,13],[23,12],[24,11]]
+		]]
 
 		if self.mode == 1:
 			for y in range(14,17):
@@ -58,7 +61,7 @@ class AlgoStrategy(gamelib.AlgoCore):
 						self.mode = 3
 					gamelib.debug_write("mode:{}".format(self.mode))
 
-		cores = game_map.get_resource("cores") - len(game_map.filter_blocked_locations(offe_locs[0][self.mode])) * game_map.type_cost(offe_locs[0][0])
+		cores = game_map.get_resource("cores") - len(game_map.filter_blocked_locations(offe_locs[0][self.mode])) * 1.5
 		for defe in defe_locs:
 			for loc in defe[self.mode]:
 				if cores >= game_map.type_cost(defe[0]):
@@ -73,13 +76,13 @@ class AlgoStrategy(gamelib.AlgoCore):
 				game_map.attempt_spawn_multiple(offe[0], offe[self.mode])
 
 			if self.mode == 3:
-				spawn_loc = [14,0]
+				loc = [14,0]
 			else:
-				spawn_loc = [13,0]
+				loc = [13,0]
 
 			while game_map.get_resource("bits") >= 1:
-				game_map.attempt_spawn("EI", spawn_loc)
-				game_map.attempt_spawn("SI", spawn_loc)
+				game_map.attempt_spawn("EI", loc)
+				game_map.attempt_spawn("SI", loc)
 
 		game_map.send_messages()
 
