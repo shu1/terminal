6af1dc39c06294fb0a6c664665ee2f5b8e55ca7f
Shuichi Aizawa <sa396@columbia.edu>
2018-08-24 05:12:38 -0400

    added mode 4 for starfishkiller

--- algo_strategy.py
+++ algo_strategy.py
@@ -31,12 +31,16 @@ class AlgoStrategy(gamelib.AlgoCore):
 		],[
 			["DF",[[16,5],[11,5],[16,7],[11,7],[16,9],[11,9],[16,11],[11,11],[15,4],[12,4],[15,6],[12,6],[15,8],[12,8],[15,10],[12,10],[15,12],[12,12],[13,11],[14,9],[13,7],[14,5],[13,3],[14,1],[16,3],[11,3]]],
 			["FF",[[15,5],[12,5],[15,7],[12,7],[15,9],[12,9],[15,11],[12,11],[15,3],[12,3]]]
+		],[
+			["DF",[[0,13],[1,13],[1,12],[2,13],[2,12],[2,11],[3,13],[3,12],[4,13]]],
+			["FF",[[24,13],[23,12],[22,11],[21,10],[20,9],[19,8],[18,7],[17,6],[16,5],[15,4],[14,3],[13,2],[12,1],[5,13],[4,12],[3,11]]]
 		]]
 		encr_locs = [
 			[[16,6],[11,6],[16,8],[11,8],[16,10],[11,10],[16,12],[11,12]],
 			[[21,10],[19,8],[17,6],[23,12]],
 			[[6,10],[8,8],[10,6],[4,12]],
-			[[16,4],[11,4],[16,6],[11,6],[16,8],[11,8],[16,10],[11,10],[16,12],[11,12]]
+			[[16,4],[11,4],[16,6],[11,6],[16,8],[11,8],[16,10],[11,10],[16,12],[11,12]],
+			[[25,13],[24,12],[23,11],[22,10],[21,9],[20,8],[19,7],[18,6],[17,5],[16,4],[15,3],[14,2],[13,1]],
 		]
 
 		if game_map.turn_number == 0:
@@ -45,40 +49,48 @@ class AlgoStrategy(gamelib.AlgoCore):
 					self.prev_wall[tuple(loc)] = True
 
 		hole = 0
-		change = False
-		for loc in wall_locs:
-			if not game_map.is_blocked(loc):
-				key = tuple(loc)
-				if self.prev_wall[key]:
-					if loc[1] <= 5:
-						self.sectors[0] += 1
-						change = True
-					elif loc[1] >= 10:
-						if loc[0] >= 14:
-							self.sectors[1] += 1
-						else:
-							self.sectors[2] += 1
-						change = True
-					self.prev_wall[key] = False
-				if game_map.attempt_spawn("DF", loc):
-					self.prev_wall[key] = True
-				else:
-					hole += 1
+		if self.mode < 4:
+			change = False
+			for loc in wall_locs:
+				if not game_map.is_blocked(loc):
+					key = tuple(loc)
+					if self.prev_wall[key]:
+						if loc[1] <= 5:
+							self.sectors[0] += 1
+							change = True
+						elif loc[1] >= 10:
+							if loc[0] >= 14:
+								self.sectors[1] += 1
+							else:
+								self.sectors[2] += 1
+							change = True
+						self.prev_wall[key] = False
+					if game_map.attempt_spawn("DF", loc):
+						self.prev_wall[key] = True
+					else:
+						hole += 1
 
-		if change:
-			if self.sectors[1] > 2 and self.sectors[1] > self.sectors[2] * 2:
-				self.mode = 1
-			elif self.sectors[2] > 2 and self.sectors[2] > self.sectors[1] * 2:
-				self.mode = 2
-			elif self.sectors[0] > 0:
+			if change:
+				if self.sectors[1] > 2 and self.sectors[1] > self.sectors[2] * 2:
+					self.mode = 1
+				elif self.sectors[2] > 2 and self.sectors[2] > self.sectors[1] * 2:
+					self.mode = 2
+				elif self.sectors[0] > 0:
+					self.mode = 3
+				elif self.sectors[1] > 2 and self.sectors[2] > 2:
+					self.mode = 0
+
+				if self.mode == 2:
+					for y in range(14,19):
+						if len(game_map.filter_blocked_locations([[10,y],[11,y],[12,y],[13,y],[14,y],[15,y],[16,y],[17,y]])) == 0:
+							game_map.attempt_remove_multiple(wall_locs)
+							self.mode = 4
+
+				gamelib.debug_write("{} MODE:{}".format(self.sectors, self.mode))
+			elif not hole and game_map.my_integrity < self.prev_health:
 				self.mode = 3
-			elif self.sectors[1] > 2 and self.sectors[2] > 2:
-				self.mode = 0
-			gamelib.debug_write("{} MODE:{}".format(self.sectors, self.mode))
-		elif not hole and game_map.my_integrity < self.prev_health:
-			self.mode = 3
-			gamelib.debug_write("{} MODE:{}".format(self.sectors, self.mode))
-		self.prev_health = game_map.my_integrity
+				gamelib.debug_write("{} MODE:{}".format(self.sectors, self.mode))
+			self.prev_health = game_map.my_integrity
 
 		defe = defe_locs[self.mode]
 		game_map.attempt_spawn_multiple(defe[0][0], defe[0][1])
@@ -89,7 +101,7 @@ class AlgoStrategy(gamelib.AlgoCore):
 			gamelib.debug_write("turn:{} health:{}".format(game_map.turn_number, int(game_map.my_integrity)))
 			game_map.attempt_spawn_multiple("EF", encr_locs[self.mode])
 
-			if self.mode == 1:
+			if self.mode == 1 or self.mode == 4:
 				loc = [13,0]
 			elif self.mode == 2:
 				loc = [14,0]
@@ -97,7 +109,11 @@ class AlgoStrategy(gamelib.AlgoCore):
 				loc = random.choice([[13,0],[14,0]])
 
 			while game_map.get_resource("bits") >= 1:
-				game_map.attempt_spawn("PI", loc)
+				if self.mode >= 4:
+					game_map.attempt_spawn("EI", loc)
+					game_map.attempt_spawn("SI", loc)
+				else:
+					game_map.attempt_spawn("PI", loc)
 
 		game_map.send_messages()
 
