17fcc804e6c73968c206fcda20096383424128e5
Shuichi Aizawa <sa396@columbia.edu>
2018-08-31 05:13:00 -0400

    move location lists from step() to init()

--- shk.py
+++ shk.py
@@ -14,9 +14,8 @@ class AlgoStrategy(gamelib.AlgoCore):
 		self.config = config
 		self.mode = 1	# hounddog
 
-	def step(self, game_map):
 		wall_locs = [[0,13],[1,12],[2,11],[3,10],[4,9],[5,8],[6,7],[7,6],[8,5],[9,4],[10,3],[11,2]]
-		defe_locs = [[
+		self.defe = [[
 			"DF",
 			[[11,11],[16,11],[12,11],[15,11],[13,11],[14,11],[12,12],[15,12],[13,12],[14,12],[13,13],[14,13]],
 			[[24,13],[23,13],[23,12],[22,13],[22,12],[22,11],[21,13],[21,12],[20,13]],
@@ -38,7 +37,7 @@ class AlgoStrategy(gamelib.AlgoCore):
 
 		filt_locs = [[24,13],[23,12],[22,11],[21,10],[20,9],[19,8],[18,7],[17,6],[16,5],[15,4],[14,3],[13,2],[12,1]]
 		encr_locs = [[25,13],[24,12],[23,11],[22,10],[21,9],[20,8],[19,7],[18,6],[17,5],[16,4],[15,3],[14,2],[13,1]]
-		offe_locs = [[
+		self.offe = [[
 			"FF",
 			filt_locs,
 			filt_locs,
@@ -52,13 +51,14 @@ class AlgoStrategy(gamelib.AlgoCore):
 			[[2,13],[3,12],[4,11],[5,10],[6,9],[7,8],[8,7],[9,6],[10,5],[11,4],[12,3],[13,2],[14,1]]
 		]]
 
+	def step(self, game_map):
 		if game_map.turn_number == 0:
 			game_map.attempt_spawn_multiple("DF", [[3,13],[24,13],[7,9],[20,9],[11,11],[16,11]])
 		else:
 			offset = 0
 			if self.mode == 1:
 				if len(game_map.filter_blocked_locations(game_map.get_edge_locations("top_left") + game_map.get_edge_locations("top_right"))) <= 4:
-					for defe in defe_locs:
+					for defe in self.defe:
 						game_map.attempt_remove_multiple(defe[self.mode])
 					self.mode = 2 # specter
 					gamelib.debug_write("MODE:{}".format(self.mode))
@@ -78,14 +78,14 @@ class AlgoStrategy(gamelib.AlgoCore):
 					gamelib.debug_write("{} {} {}".format(paths, paths[x], xs))
 
 					if x < 5:
-						for defe in defe_locs:
+						for defe in self.defe:
 							game_map.attempt_remove_multiple(defe[self.mode])
 						self.mode = 3	# starfish
 						gamelib.debug_write("MODE:{}".format(self.mode))
 					elif x > 22:
-						for defe in defe_locs:
+						for defe in self.defe:
 							game_map.attempt_remove_multiple(defe[self.mode])
-						for offe in offe_locs:
+						for offe in self.offe:
 							game_map.attempt_remove_multiple(offe[self.mode])
 						game_map.attempt_remove_multiple([[2,12],[3,12],[3,11],[4,11]])
 						self.mode = 4	# champ
@@ -95,8 +95,8 @@ class AlgoStrategy(gamelib.AlgoCore):
 						if offset > 5:
 							offset = 5
 
-			cores = game_map.get_resource("cores") - len(game_map.filter_blocked_locations(offe_locs[0][self.mode])) * 1.5
-			for i, defe in enumerate(defe_locs):
+			cores = game_map.get_resource("cores") - len(game_map.filter_blocked_locations(self.offe[0][self.mode])) * 1.5
+			for i, defe in enumerate(self.defe):
 				for loc in defe[self.mode]:
 					if cores >= game_map.type_cost(defe[0]):
 						if self.mode == 1 and i < 2:	# floating island
@@ -108,7 +108,7 @@ class AlgoStrategy(gamelib.AlgoCore):
 			gamelib.debug_write("{} health:{} enemy:{}".format(game_map.turn_number, int(game_map.my_integrity), int(game_map.enemy_integrity)))
 
 			if game_map.turn_number > 0:
-				for offe in offe_locs:
+				for offe in self.offe:
 					game_map.attempt_spawn_multiple(offe[0], offe[self.mode])
 
 			loc = [14,0] if self.mode == 4 else [13,0]
